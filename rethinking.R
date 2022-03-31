0131bc90fd16e62891159626d89eb2c303a469a9
MICHAEL J ZENZ <zenz@wisc.edu>
2022-03-31 01:01:01 -0500

    Completed up to 5.2

--- rethinking.R
+++ rethinking.R
@@ -172,6 +172,7 @@ cov2cor(vcov(m4.1))
 post <- extract.samples(m4.1, n=100000)
 head(post)
 
+
 xbar <- mean(d2$weight)
 
 
@@ -343,3 +344,152 @@ d$D <- standardize(d$Divorce)
 d$M <- standardize(d$Marriage)
 d$A <- standardize(d$MedianAgeMarriage)
 
+#Chapter 5
+data(WaffleDivorce)
+d <- WaffleDivorce
+
+d$D <- standardize(d$Divorce)
+d$M <- standardize(d$Marriage)
+d$A <- standardize(d$MedianAgeMarriage)
+
+sd(d$MedianAgeMarriage)
+
+m5.1 <- quap(
+  alist(
+    D~dnorm(mu,sigma),
+    mu <- a +bA*A,
+    a ~ dnorm(0,.2),
+    bA~dnorm(0, .5),
+    sigma~dexp(1)),
+  data = d)
+  
+set.seed(10)
+prior <- extract.prior(m5.1)
+mu <- link(m5.1, post=prior, data = list(A=c(-2,2)))
+plot(NULL, xlim=c(-2,2), ylim=c(-2,2))
+for(i in 1:50) lines(c(-2,2), mu[i,], col=col.alpha("black", 0.4))
+
+#pg 127
+A_seq <- seq(from = -3, to = 3.2, length.out = 30)
+mu <- link(m5.1, data = list(A=A_seq))
+mu.mean <- apply( mu, 2, mean)
+mu.PI <- apply(mu,2, PI)
+
+plot(D ~ A, data = d, col = rangi2)
+lines(A_seq, mu.mean, lwd=2)
+shade(mu.PI, A_seq)
+
+m5.2 <- quap(
+  alist(
+    D ~ dnorm(mu, sigma),
+    mu <- a + bM * M,
+    a ~ dnorm(0, .2),
+    bM ~ dnorm(0, .5),
+    sigma ~ dexp(1)),
+  data = d)
+
+M_seq <- seq(from = -3, to = 3.2, length.out = 30)
+mu <- link(m5.2, data = list(M=M_seq))
+mu.mean <- apply( mu, 2, mean)
+mu.PI <- apply(mu,2, PI)
+
+plot(D ~ M, data = d, col = rangi2, xlim = c(-2.5,2.5))
+lines(M_seq, mu.mean, lwd=2)
+shade(mu.PI, M_seq)
+
+#pg 133
+m5.3 <- quap(
+  alist(
+      D ~ dnorm(mu, sigma),
+      mu <- a + bM * M + bA*A,
+      a ~ dnorm(0, .2),
+      bM ~dnorm(0, .5),
+      bA ~dnorm(0, .5),
+      sigma ~ dexp(1) ),
+  data = d)
+precis(m5.3)
+plot(coeftab(m5.1, m5.2, m5.3), par=c("bA", "bM"))
+
+
+#pg. 134
+N <- 50
+age <- rnorm(N)
+mar <- rnorm(N, -age)
+div <- rnorm(N, age)
+
+m5.4 <- quap(
+    alist(
+      M ~ dnorm(mu, sigma), 
+      mu <- a + bAM * A,
+      a ~ dnorm(0, .2),
+      bAM ~ dnorm(0, .5),
+      sigma ~ dexp(1)),
+    data = d)
+
+#pg 136
+mu <- link(m5.4)
+mu_mean <- apply(mu, 2, mean)
+mu_resid <- d$M - mu_mean
+
+#pg 138
+mu <- link(m5.3)
+mu_mean <- apply(mu, 2, mean)
+mu_PI <- apply(mu, 2, PI)
+
+D_sim <- sim(m5.3, n=10000)
+D_PI <- apply(D_sim, 2, PI)
+plot(mu_mean ~ d$D, col=rangi2, ylim = range(mu_PI),
+     xlab = "Observed divorce", ylab = "Predicted divorce")
+abline(a=0,b=1,lty=2)
+for(i in 1:nrow(d)) lines(rep(d$D[i], 2), mu_PI[,i], col=rangi2)
+
+identify(x = d$D, y = mu_mean, labels = d$Loc)
+
+#pg 141
+rm(list = ls())
+
+data(WaffleDivorce)
+d <- list()
+d$A <- standardize(WaffleDivorce$MedianAgeMarriage)
+d$D <- standardize(WaffleDivorce$Divorce)
+d$M <- standardize(WaffleDivorce$Marriage)
+
+m5.3_A <- quap(
+  alist(
+    ## A -> D <- M
+    D ~ dnorm(mu, sigma),
+    mu <- a + bM*M + bA*A,
+    a ~ dnorm(0, .2),
+    bM ~ dnorm(0, .5),
+    bA ~ dnorm(0, .5),
+    sigma ~ dexp(1),
+    ## A -> M
+    M ~ dnorm(mu_M, sigma_M),
+    mu_M <- aM + bAM*A,
+    aM ~ dnorm(0, .2),
+    bAM ~ dnorm(0, .5),
+    sigma_M ~ dexp(1)),
+      data = d)
+
+A_seq <- seq(from = -2, to = 2, length.out = 30)
+
+sim_dat <- data.frame(A=A_seq)
+s <- sim(m5.3_A, data = sim_dat, vars=c("M", "D"))
+
+#pg 142
+plot(sim_dat$A, colMeans(s$D), ylim = c(-2,2), type = "l",
+     xlab="manipulated A", ylab="counterfactual D")
+shade(apply(s$D, 2, PI), sim_dat$A)
+mtext("Total counterfactual effect of A on D")
+
+sim2_dat <- data.frame(A = (c(20,30) - 26.1)/1.24)
+s2 <- sim(m5.3_A, data = sim2_dat, vars=c("M", "D"))
+mean(s2$D[,2] - s2$D[,1])
+
+#pg 143
+sim_dat <- data.frame(M=seq(from = -2, to = 2, length.out = 30), A = 0)
+s <- sim(m5.3_A, data = sim_dat, vars = "D")
+plot(sim_dat$M, colMeans(s), ylim=c(-2,2), type = "l",
+     xlab = "manipulated M", ylab = "counterfactual D")
+shade(apply(s,2,PI), sim_dat$M)
+mtext("Total counterfactual effect of M on D")
